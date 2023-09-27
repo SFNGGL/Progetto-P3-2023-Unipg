fb50b09e55601d70cea07dc63302d1564a1995b0
SFNGGL <stefanogigligigli@gmail.com>
2023-09-27 12:21:36 +0200

    creati component necessari

---
+++ src/app/components/endscreen/endscreen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-endscreen',
+  templateUrl: './endscreen.component.html',
+  styleUrls: ['./endscreen.component.css']
+})
+export class EndscreenComponent {
+
+}
