48982a6876022a0627ffa4ffa1e45de24a0b0fe7
SFNGGL <stefanogigligigli@gmail.com>
2023-10-06 18:08:32 +0200

    firebase creds

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: '',
-    appId: '',
-    databaseURL: '',
-    storageBucket: '',
-    apiKey: '',
-    authDomain: '',
-    messagingSenderId: '',
+    apiKey: "AIzaSyBWBekirzzoUjqIIC79iAgfFti9xPnQJsM",
+    authDomain: "progettosdep2023.firebaseapp.com",
+    databaseURL: "https://progettosdep2023-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "progettosdep2023",
+    storageBucket: "progettosdep2023.appspot.com",
+    messagingSenderId: "181580003123",
+    appId: "1:181580003123:web:79c9caf0fd9d327789b046"
   }
 };
