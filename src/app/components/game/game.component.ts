0d23c139be127c961291b8df10bd11d5248f127e
SFNGGL <stefanogigligigli@gmail.com>
2023-10-12 12:44:11 +0200

    added p5 library and types

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as p5 from 'p5';
+import "p5/lib/addons/p5.sound"
+import "p5/lib/addons/p5.dom"
 
 @Component({
   selector: 'app-game',
@@ -9,5 +12,25 @@ export class GameComponent implements OnInit {
   // public functions go here
   // 947
 
-  ngOnInit(): void { }
+  private p5: any;
+
+  ngOnInit(): void {
+    this.createCanvas();
+  }
+
+  private createCanvas() {
+    this.p5 = new p5(this.sketch);
+  }
+
+  private sketch(p: any) {
+    p.setup = () => {
+      p.createCanvas(700, 600);
+    };
+
+    p.draw = () => {
+      p.background(255);
+      p.fill(0);
+      p.rect(p.width / 2, p.height / 2, 50, 50);
+    };
+  }
 }
--- src/assets/basefiles/sketch.js
+++ src/assets/basefiles/sketch.js
@@ -11,100 +11,100 @@ var nuovoColore = "";
 var gameIndex = 0;
 
 
-function preload(){
-    miniGames.push(gioco_liz);
-    miniGames[gameIndex].loadRes();
+function preload() {
+  // miniGames.push(gioco_liz);
+  // miniGames[gameIndex].loadRes();
 }
 
 function setup() {
 
-    body = select("body");
-    canvas = createCanvas(body.width, body.height);
-    calcScaleAttr(body.width, body.height);
+  body = select("body");
+  canvas = createCanvas(body.width, body.height);
+  calcScaleAttr(body.width, body.height);
 
-    miniGames[gameIndex].constructor();
+  miniGames[gameIndex].constructor();
 }
 
 function draw() {
-    translate(getTranslation().x,getTranslation().y);
-    scale(getScale().w,getScale().h);
+  translate(getTranslation().x, getTranslation().y);
+  scale(getScale().w, getScale().h);
 
-    miniGames[gameIndex].draw(Canvas_resized);
-    Canvas_resized = false;
+  miniGames[gameIndex].draw(Canvas_resized);
+  Canvas_resized = false;
 }
 
-function keyPressed(){
-    if(miniGames[gameIndex].keyPressed !== undefined){
-        miniGames[gameIndex].keyPressed();
-    }
+function keyPressed() {
+  if (miniGames[gameIndex].keyPressed !== undefined) {
+    miniGames[gameIndex].keyPressed();
+  }
 
-    if(keyCode == ESCAPE) {
-      quitGame();
-    }
+  if (keyCode == ESCAPE) {
+    quitGame();
+  }
 }
 
 function quitGame() {
   window.location.href = "index.html";
 }
 
-function mousePressed(){
-    if(miniGames[gameIndex].mousePressed !== undefined){
-        miniGames[gameIndex].mousePressed();
-    }
+function mousePressed() {
+  if (miniGames[gameIndex].mousePressed !== undefined) {
+    miniGames[gameIndex].mousePressed();
+  }
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function gameCompleted(success){
+async function gameCompleted(success) {
 
-    if(success){
-        confirm("YOU DID IT! DO YOU WANNA PLAY AGAIN?");
-        miniGames[gameIndex].constructor();
-    }
-    else{
-        confirm("RIP, DO YOU WANNA PLAY AGAIN?");
-        miniGames[gameIndex].constructor();
-    }
+  if (success) {
+    confirm("YOU DID IT! DO YOU WANNA PLAY AGAIN?");
+    miniGames[gameIndex].constructor();
+  }
+  else {
+    confirm("RIP, DO YOU WANNA PLAY AGAIN?");
+    miniGames[gameIndex].constructor();
+  }
 }
-   
+
 
 function setup() {
 
-    body = select("body");
-    canvas = createCanvas(body.width, body.height);
-    calcScaleAttr(body.width, body.height);
+  body = select("body");
+  canvas = createCanvas(body.width, body.height);
+  calcScaleAttr(body.width, body.height);
 
-    miniGames[gameIndex].constructor();
+  miniGames[gameIndex].constructor();
 }
 
 function draw() {
-    translate(getTranslation().x,getTranslation().y);
-    scale(getScale().w,getScale().h);
+  translate(getTranslation().x, getTranslation().y);
+  scale(getScale().w, getScale().h);
 
-    miniGames[gameIndex].draw(Canvas_resized);
-    Canvas_resized = false;
+  miniGames[gameIndex].draw(Canvas_resized);
+  Canvas_resized = false;
 }
 
-function keyPressed(){
-    if(miniGames[gameIndex].keyPressed !== undefined){
-        miniGames[gameIndex].keyPressed();
-    }
+function keyPressed() {
+  if (miniGames[gameIndex].keyPressed !== undefined) {
+    miniGames[gameIndex].keyPressed();
+  }
 
-    if(keyCode == ESCAPE) {
-      quitGame();
-    }
+  if (keyCode == ESCAPE) {
+    quitGame();
+  }
 }
 
 function quitGame() {
   window.location.href = "index.html";
 }
 
-function mousePressed(){
-    if(miniGames[gameIndex].mousePressed !== undefined){
-        miniGames[gameIndex].mousePressed();
-    }
+function mousePressed() {
+  if (miniGames[gameIndex].mousePressed !== undefined) {
+    miniGames[gameIndex].mousePressed();
+  }
 }
 
 function sleep(ms) {
@@ -123,7 +123,7 @@ function addVictoryToDb(gameName, c1, c2) {
   var request = $.ajax({
     url: "database.php",
     method: "POST",
-    data: { game: gameName, color1: c1, color2: c2, action : 'victory' },
+    data: { game: gameName, color1: c1, color2: c2, action: 'victory' },
     dataType: "json",
     success: function(msg) {
       nuovoColore = msg;
