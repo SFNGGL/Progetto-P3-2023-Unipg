6dbd0b68f7b30436fcfc14a7237277a63333931b
SFNGGL <stefanogigligigli@gmail.com>
2023-10-15 18:29:28 +0200

    developed working though rough base, needs game for standard base and proper score save system

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
 import * as p5 from 'p5';
 import "p5/lib/addons/p5.sound"
-import "p5/lib/addons/p5.dom"
+import { Component, OnInit, importProvidersFrom } from '@angular/core';
+import { VehiclesComponent } from "../vehicles/vehicles.component";
+import { FunctionalitiesComponent } from "../functionalities/functionalities.component";
+import { EndscreenComponent } from "../endscreen/endscreen.component";
 
 @Component({
   selector: 'app-game',
@@ -12,25 +14,75 @@ export class GameComponent implements OnInit {
   // public functions go here
   // 947
 
-  private p5: any;
+  private FRICTION = 0.004;
+  private ACCELERATION = 15;
+  private GRAVITY = -0.2;
+  private CAR_EXP_LATENCY = -150;
+  private EXP_LETAL_TIME = 150;
+  private SQRT = Math.sqrt(2);
+  private TIME = 91;
+
+  img = {};
+  sound = {};
+  seicento: any;
+  courseMap: any = undefined;
+
+  nemici: any;
+  cars: any;
+  explosions: any;
+  bullets: any;
+  pgDied = false;
+  seicentoLoaded = false;
+  dontDraw = false;
+  slider: any = null;
+  onlyWinOnce = false;
+  timer: any;
+  vecchiSpawnTimer: any;
+  stop: any;
+
+  constructor(
+    public car_c: VehiclesComponent,
+    public func_c: FunctionalitiesComponent,
+    public endscreen: EndscreenComponent
+  ) {
+    this.img = {};
+    this.sound = {};
+    this.seicento;
+    this.courseMap = undefined;
+
+    this.nemici;
+    this.cars;
+    this.explosions;
+    this.bullets;
+    this.pgDied = false;
+    this.seicentoLoaded = false;
+    this.dontDraw = false;
+    this.slider = null;
+    this.onlyWinOnce = false;
+    this.timer;
+    this.vecchiSpawnTimer;
+    this.stop;
+  }
 
   ngOnInit(): void {
-    this.createCanvas();
+    const sketch = (s: any) => {
+
+      s.preload = () => {
+        // preload code
+      }
+
+      s.setup = () => {
+        s.createCanvas(400, 400);
+      }
+
+      s.draw = () => {
+        s.background(51);
+        s.rect(100, 100, 100, 100);
+      }
+    }
+    let canvas = new p5(sketch)
   }
 
-  private createCanvas() {
-    this.p5 = new p5(this.sketch);
-  }
+  // this.car_c.car
 
-  private sketch(p: any) {
-    p.setup = () => {
-      p.createCanvas(700, 600);
-    };
-
-    p.draw = () => {
-      p.background(255);
-      p.fill(0);
-      p.rect(p.width / 2, p.height / 2, 50, 50);
-    };
-  }
 }
--- src/app/components/vehicles/vehicles.component.ts
+++ src/app/components/vehicles/vehicles.component.ts
@@ -1,4 +1,7 @@
+import * as p5 from 'p5';
+import "p5/lib/addons/p5.sound"
 import { Component } from '@angular/core';
+import { FunctionalitiesComponent } from "../functionalities/functionalities.component";
 
 @Component({
   selector: 'app-vehicles',
@@ -9,4 +12,32 @@ export class VehiclesComponent {
 
   constructor() { }
 
+  car = class Car {
+
+  }
+
+  wheels = class Wheels {
+    x: number;
+    y: number;
+    r: number;
+    vx: number;
+    vy: number;
+    a: number;
+    angle: number;
+    constructor(
+      x: number,
+      y: number,
+      r: number
+    ) {
+      this.x = x;
+      this.y = y;
+      this.r = r;
+      this.vx = 1;
+      this.vy = 1;
+      this.a = 0;
+      this.angle = 0;
+    }
+  }
+
 }
+
